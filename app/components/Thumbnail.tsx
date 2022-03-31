75d8899af035fd1a0bdd484af2bb68e9f793e55d
Lance Klinger <lance.d.klinger@gmail.com>
2022-03-31 18:28:54 -0400

    Removing default code and adding initial pages

---
+++ app/components/Thumbnail.tsx
@@ -0,0 +1,13 @@
+
+const Thumbnail = ({ title, image, price }: { title: string, image: string, price: string }) => {
+  return (
+    <div className="pl-3">
+      <img className="inline" src={image} alt="Blueberries" width="100px" />
+      <div className="inline font-medium">{title}</div>
+      <div className="ml-2 inline">${price}</div>
+      <button className="bg-blue-500 text-white p-2 ml-2" onClick={() => alert(`Added ${title} to cart`)}>Add to cart</button>
+    </div>
+  );
+}
+
+export default Thumbnail;
